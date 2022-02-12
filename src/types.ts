8391756ff2e6c5cfc63bc1304b13f19a592f2795
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-02-12 00:52:42 -0500

    Revisited

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,47 @@
-export interface IWindow {
-  
+export interface Window {
+  location: URL
 }
 
-export interface INode {
-  
+export interface Document {
+
 }
 
-export interface IElement {
+export interface Node {
+  childNodes: NodeList
+  cloneNode: (deep?: boolean) => Node
+}
+
+export interface NodeList extends Array<Node>{
+  item: (index: number) => Node | null
+}
+
+export enum NodeType {
+  ELEMENT_NODE = 1,
+  ATTRIBUTE_NODE = 2,
+  TEXT_NODE = 3,
+  PROCESSING_INSTRUCTION_NODE = 7,
+  COMMENT_NODE = 8,
+  DOCUMENT_NODE = 9,
+  DOCUMENT_TYPE_NODE = 10,
+  DOCUMENT_FRAGMENT_NODE = 11
+}
+
+export interface Element {
   getAttribute: (name: string) => string
   setAttribute: (name: string, value: string) => void
+}
+
+export interface Event {
+  type: string
 }
 
 export interface Settings {
   baseURL: string
   userAgent?: string
   platform?: string
-  querySelectorAll?: (root: INode, selector: string) => INode[]
+  querySelectorAll?: (root: Node, selector: string) => Node[]
+}
+
+export function impl<T, I> (obj: T): I {
+  return obj as unknown as I
 }
